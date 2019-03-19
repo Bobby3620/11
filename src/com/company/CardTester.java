5cdb1dfdf5b08bb3d1bd03b6314c410c8b8d84da
rd568 <rd568@pvsd.org>
2019-03-19 11:16:50 -0400

    Initial commit

---
+++ src/com/company/CardTester.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * This is a class that tests the Card class.
+ */
+
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+
+        Card c = new Card("Fourth", "Cool", 4);
+        Card d = new Card("Fourth", "Cool", 4);
+
+
+
+        System.out.println(c.matches(d));
+
+    }
+}
